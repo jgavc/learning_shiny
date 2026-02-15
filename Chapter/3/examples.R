fd8cfaabcfa9fea23f8c86437678c291d0f7b344
jccoolguy PC <jgavc1@gmail.com>
2026-02-15 16:17:54 -0500

    starting chapter 3

---
+++ Chapter/3/examples.R
@@ -0,0 +1,15 @@
+library(shiny)
+library(ggplot2)
+
+
+ui <- fluidPage(
+  textInput("name", "What's your name?"),
+  textOutput("greeting")
+)
+
+server <- function(input, output, session) {
+  output$greeting <- renderText({
+    paste0("Hello ", input$name, "!")
+  })
+}
+shinyApp(ui, server)
---
+++ Exercises/03.03.06/2.R
@@ -0,0 +1,14 @@
+library(shiny)
+library(ggplot2)
+
+
+ui <- fluidPage(
+  textInput("name", "What's your name?"),
+  textOutput("greeting")
+)
+
+server <- function(input, output, server) {
+  output$greeting <- renderText(paste0("Hello ", input$name))
+}
+
+shinyApp(ui, server)
