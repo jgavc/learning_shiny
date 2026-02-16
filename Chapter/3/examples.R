f991986092b0fb9df27dfc95983f28a42b731a51
jccoolguy PC <jgavc1@gmail.com>
2026-02-15 23:10:07 -0500

    completed chapter 3 02/15

--- Chapter/3/examples.R
+++ Chapter/3/examples.R
@@ -1,15 +1,17 @@
 library(shiny)
 library(ggplot2)
 
-
 ui <- fluidPage(
   textInput("name", "What's your name?"),
   textOutput("greeting")
 )
 
 server <- function(input, output, session) {
-  output$greeting <- renderText({
-    paste0("Hello ", input$name, "!")
+  string <- reactive(paste0("Hello ", input$name, "!"))
+  
+  output$greeting <- renderText(string())
+  observeEvent(input$name, {
+    message("Greeting performed")
   })
 }
 shinyApp(ui, server)
